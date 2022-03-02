5e913814b11d8431aa69b28ea98b1bb049a2ec00
Roman Pchelkin <epos.91@mail.ru>
2022-03-02 10:31:44 +0300

    Create lesson78.js

---
+++ les/lesson78.js
@@ -0,0 +1,23 @@
+//Библиотека JQuery
+
+//Подлючение к проекту, cdnjs сайт
+
+//В терминале npm i jquery --save
+import $ from 'jquery';
+
+const btn = $('#btn'); //Обращение к элементу на странцие
+console.log(btn);
+
+$(document).ready(function () { //Дожидаемся загрузки всей страницы
+	$('.list-item:first').hover(function () { //Выбор только первого элемента 
+		$(this).toggleClass('active');
+	});
+
+	$('.list-item:eq(2)').on('click', function () {
+		$('.image:even').fadeToggle('slow'); //Анимация медленного скрытия элемента
+	});
+
+	$('.list-item:eq(4)').on('click', function () {
+		$('.image:odd').animate();
+	});
+}); 
